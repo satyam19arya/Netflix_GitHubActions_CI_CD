e27968b7fad90161a556ce386948c74bbf24b666
Satyam <satyam19arya@gmail.com>
2024-02-29 13:44:50 +0530

    updated

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,21 @@
+import Box from "@mui/material/Box";
+
+export default function Footer() {
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: 150,
+        bgcolor: "inherit",
+        px: "60px",
+      }}
+    >
+    </Box>
+  );
+}
